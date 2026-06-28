e7ad54b9383a988ceabb087d707db6d195b4f849
maing1 <faithmaingi1622@gmail.com>
2026-06-28 17:05:12 +0300

    Fixed add to cart function

--- script.js
+++ script.js
@@ -1,24 +1,20 @@
+const contactForm = document.getElementById("contact-form");
 // Contact Form Validation
 
+if (contactForm) {
 document.getElementById("contact-form").addEventListener("submit", function(event) {
-
     event.preventDefault();
-
     let isValid = true;
-
     // Get form values
     const name = document.getElementById("f-name").value.trim();
     const email = document.getElementById("f-email").value.trim();
     const phone = document.getElementById("f-tel").value.trim();
     const message = document.getElementById("f-message").value.trim();
-
     // Message box
     const output = document.getElementById("js-message");
-
     // Clear previous message
     output.textContent = "";
     output.className = "";
-
     // Validation
     if (name === "") {
         isValid = false;
@@ -55,25 +51,49 @@ document.getElementById("contact-form").addEventListener("submit", function(even
     }
 
 });
+}
 
 // Add to Cart
-let cart = [];
+let cart = JSON.parse(localStorage.getItem("cart")) || [];
 
 function addToCart(productName) {
-
     cart.push(productName);
-
+    localStorage.setItem("cart", JSON.stringify(cart));
     document.getElementById("cart-count").textContent = cart.length;
-
     alert(productName + " added to cart!");
 }
 
+function loadCart() {
+
+    const cartItems = document.getElementById("cart-items");
+
+    if (!cartItems) return;
+
+    const cart = JSON.parse(localStorage.getItem("cart")) || [];
+
+    cartItems.innerHTML = "";
+
+    cart.forEach(function(item) {
+
+        cartItems.innerHTML +=
+            "<li class='list-group-item'>" +
+            item +
+            "</li>";
+
+    });
+
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+
+    loadCart();
+
+});
+
+
 //Add to wishlist
 let wishlist = [];
-
 function addToWishlist(productName) {
-
     wishlist.push(productName);
-
     alert(productName + " added to wishlist!");
 }
